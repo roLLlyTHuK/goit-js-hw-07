8132618bf09aa1af66a5210fdfb37bb1e8bd0f88
roLLlyTHuK <ircvipua@gmail.com>
2023-08-24 18:56:41 +0300

    +task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryList = document.querySelector(`.gallery`);
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
 
+//! створення галлереї
+function createGalleryMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+        />
+        </a>
+        </li>
+        `;
+    }).join('');
+} 
+//! підключення бібліотеки з параметрами
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt', 
+    captionDelay: 250,
+    close: false,
+    enableKeyboard: true,
+   
+   
+});
 console.log(galleryItems);
